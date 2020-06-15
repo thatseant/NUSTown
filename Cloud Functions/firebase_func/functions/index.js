f603dcdce63e369211365b3e7d80b8ddee279f47
thatseant <46109197+thatseant@users.noreply.github.com>
2020-06-15 23:21:09 +0800

    Filters Instagram posts for events
    Lines 53 to 68: Extract event dates from caption.
    
    Line 71: Checks that this event date is after the post date. [Future Event]. Thus, only posts about future events are stored in database.
    
    Line 112 to 114: Prevents duplicates. Does not create new event if document with same ID already exists.

--- Cloud Functions/firebase_func/functions/index.js
+++ Cloud Functions/firebase_func/functions/index.js
@@ -6,7 +6,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 const os = require('os');
 const fs = require("fs");
-const serviceAccount = require("./NUSTown-984db999f43e.json");
+const serviceAccount = require("./NUSTown-ffc8c62cae11");
 const { v4: uuidv4 } = require('uuid');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -15,6 +15,7 @@ admin.initializeApp({
 const axios = require('axios').default;
 const bucket = admin.storage().bucket();
 const moment = require('moment');
+var chrono = require('chrono-node');
 
 exports.createEvent = functions.https.onRequest(async (req, res) => {
 
@@ -45,57 +46,108 @@ async function createProfileEvents (userID, numberOfPosts) {
 
 async function tasks(allMedia, i, userID) {
     var id, cat, name, place, url;
+    var firstDate, firstText;
     let imageURL = allMedia.edges[i].node.display_resources[2].src //get url for ith image
     let caption = allMedia.edges[i].node.edge_media_to_caption.edges[0].node.text
     let date = moment.unix(allMedia.edges[i].node.taken_at_timestamp)
-    if (userID === "3585406095") {
-        cat = "Sports Events"
-        name = "Some Kayak Event"
-        place = "UTown"
-        url = "https://www.instagram.com/nuskayaking/"
-        id = "kayak_" + date
-    }
-    else if (userID === "2280311232") {
-        cat = "NUS CCAs"
-        name = "Some Climb Event"
-        place = "USC"
-        url = "https://www.instagram.com/nus_climbing/"
-        id = "climb_" + date
-    }
-    else if (userID === "1921006487") {
-        cat = "Open Classes"
-        name = "Some Dance Event"
-        place = "NUS-Wide"
-        url = "https://www.instagram.com/breakinus/"
-        id = "dance_" + date
-    }
-    else {
-        cat = "Other Events"
-        id = "other_" + date
-    }
-    let newDoc = {
-        image: id + ".png",
-        info: caption,
-        time: moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a"),
-        category: cat,
-        numberAttending: 0,
-        name: name,
-        place: place,
-        rating: 3,
-        url: url,
-        id: id
-    }
+    let results = await chrono.strict.parse(caption, date.toDate())
+    let casualResults = await chrono.parse(caption, date.toDate())
 
-    await admin.firestore().collection('events').doc(id).set(newDoc)
-
-    const picName = id + ".png";
-    const tempFilePath = path.join(os.tmpdir(), picName);
-    await download(imageURL, tempFilePath).then(() => bucket.upload(tempFilePath,
-        {destination: "events/"+ picName,  metadata: {metadata :{
-                    firebaseStorageDownloadTokens: uuidv4(),
+    if (results.length!==0) {
+        for (j = 0; j < results.length; j++) {
+            if (results[j].text !== "9/20") {
+                if (results[j].tags) {
+                    if (results[j].tags.ENMonthNameParser) {
+                        continue
+                    }
                 }
-            },}))
+                firstDate = results[j].start.date()
+                firstText = results[j].text
+                break;
+            }
+        }
+    }
+
+    if (firstDate >= date.toDate()) {//Only runs below if post is a valid event
+        stringDate = moment(firstDate).format("YYMMDD");
+        if (userID === "3585406095") {
+            cat = "Sports Events"
+            name = "Some Kayak Event"
+            place = "UTown"
+            url = "https://www.instagram.com/nuskayaking/"
+            id = "kayak_" + stringDate
+        }
+        else if (userID === "2280311232") {
+            cat = "NUS CCAs"
+            name = "Some Climb Event"
+            place = "USC"
+            url = "https://www.instagram.com/nus_climbing/"
+            id = "climb_" + stringDate
+        }
+        else if (userID === "1921006487") {
+            cat = "Open Classes"
+            name = "Some Dance Event"
+            place = "NUS-Wide"
+            url = "https://www.instagram.com/breakinus/"
+            id = "dance_" + stringDate
+        }
+        else {
+            cat = "Other Events"
+            id = "other_" + stringDate
+        }
+        let newDoc = {
+            image: id + ".png",
+            info: caption,
+            time: moment(firstDate).format("dddd, MMMM Do YYYY, h:mm:ss a"),
+            category: cat,
+            numberAttending: 0,
+            name: name,
+            place: place,
+            rating: 3,
+            url: url,
+            id: id
+        }
+
+
+        await admin.firestore().collection('events').doc(id).get().then(
+            async (doc) => {
+                if (!doc.exists) {
+                    await admin.firestore().collection('events').doc(id).set(newDoc)
+                    const picName = id + ".png";
+                    const tempFilePath = path.join(os.tmpdir(), picName);
+                    await download(imageURL, tempFilePath).then(() => bucket.upload(tempFilePath,
+                        {destination: "events/"+ picName,  metadata: {metadata :{
+                                    firebaseStorageDownloadTokens: uuidv4(),
+                                }
+                            },}))
+                }
+                return null;
+            }
+        )
+    }
+
 }
+
+// await db.runTransaction(t => { //transaction method
+//     return t.get(docRef)
+//         .then(doc => {
+//             if (!doc.exists) {
+//                 t.set(docRef, newDoc)
+//             } else {
+//
+//             }
+//         });
+// }).then(async result => {
+//     const picName = id + ".png";
+//     const tempFilePath = path.join(os.tmpdir(), picName);
+//     await download(imageURL, tempFilePath).then(() => bucket.upload(tempFilePath,
+//         {destination: "events/"+ picName,  metadata: {metadata :{
+//                     firebaseStorageDownloadTokens: uuidv4(),
+//                 }
+//             },}))
+// }).catch(err => {
+//     console.log('Transaction failure:', err);
+// });
 
 async function download (newURL, downloadPath) {
     const url = newURL
